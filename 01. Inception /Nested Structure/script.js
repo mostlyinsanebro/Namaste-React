87d3df7c84016474319e9bf9ae9902dd0d20d4f6
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-11-21 11:32:56 +0530

    Update script.js
    Creating a complex nested structure using createElement() in React

--- 01. Inception /Nested Structure/script.js
+++ 01. Inception /Nested Structure/script.js
@@ -2,23 +2,33 @@
 
 /*
 <div id = 'parent>
-  <div id = 'child>
+  <div id = 'child'>
       <h1> I am an H1 tag </h1>
+      <h1> I am an H2 tag </h2>
+  </div>
+    <div id = 'child2'>
+      <h1> I am an H1 tag </h1>
+      <h1> I am an H2 tag </h2>
   </div>
 </div>
 */
 
-const heading = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I am an H1 tag")
-  )
-);
+// createElement(type, props, ...children)
+
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H2 tag"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(heading);
 
 root.render(heading);
 // This line first takes in the heading object as input and then converts in into HTML and then
