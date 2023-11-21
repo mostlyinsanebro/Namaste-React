d78f842657703810311125fd83ffb687cdb4dfa6
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-11-21 11:19:46 +0530

    Update script.js

--- 01. Inception /Nested Structure/script.js
+++ 01. Inception /Nested Structure/script.js
@@ -21,3 +21,5 @@ const heading = React.createElement(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+// This line first takes in the heading object as input and then converts in into HTML and then
+// puts it up on the HTML page.
