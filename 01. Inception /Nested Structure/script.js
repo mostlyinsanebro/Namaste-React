b2260b51cde21adeeb19d3971d77f58788ef931d
Abhinav <67469292+mostlyinsanebro@users.noreply.github.com>
2023-11-21 11:18:54 +0530

    Create script.js

---
+++ 01. Inception /Nested Structure/script.js
@@ -0,0 +1,23 @@
+// Creating a nested structure in HTML document using React.
+
+/*
+<div id = 'parent>
+  <div id = 'child>
+      <h1> I am an H1 tag </h1>
+  </div>
+</div>
+*/
+
+const heading = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am an H1 tag")
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
